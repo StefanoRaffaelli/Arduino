b45b5023cac6949b8289c1c86bee25a0fd428953
StefanoRaffaelli <stefano.raffaelli1977@gmail.com>
2020-06-20 15:07:14 +0200

    Piccolo sample di PIR motion sensor

---
+++ Sketches/PIR_Motion_sensor/PIR_Motion_sensor.ino
@@ -0,0 +1,18 @@
+int sensor = 13;  // Digital pin D7
+
+void setup() {
+  pinMode(sensor, INPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  long state = digitalRead(sensor);
+  if (state == HIGH) {
+    Serial.println("Motion detected!");
+    delay(1000);
+  }
+  else {
+    Serial.println("No motion detected.");
+    delay(1000);
+  }
+}
